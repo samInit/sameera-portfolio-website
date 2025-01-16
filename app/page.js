0067809f942cf9ceb4c553acc8f8544839890af2
sameera9304 <kusalasameera@gmail.com>
2025-01-17 00:33:53 +0530

    [feature] navbar added

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
-      </div>
-
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <Navbar />
+        <div className="container mt-24 mx-auto px-12 py-4">
+          <HeroSection />
+        </div>
+      </main>
     </main>
   );
 }
