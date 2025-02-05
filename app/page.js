685fa28fbfa916991f9f41401525be32108f9f1a
sameera9304 <kusalasameera@gmail.com>
2025-02-05 09:13:44 +0530

    [add] email section + footer

--- app/page.js
+++ app/page.js
@@ -4,6 +4,9 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
@@ -13,7 +16,9 @@ export default function Home() {
           <HeroSection />
           <AboutSection />
           <ProjectsSection />
+          <EmailSection />
         </div>
+        <Footer />
       </main>
     </main>
   );
